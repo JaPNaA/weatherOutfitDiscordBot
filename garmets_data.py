6700dd89688e106ad4189bbf0bfaf72e96e0edf3
JaPNaA <leonehuang100@gmail.com>
2022-04-30 14:39:47 -0400

    Reformat outfits data

--- garmets_data.py
+++ garmets_data.py
@@ -5,19 +5,79 @@ from typing import List
 class Outfit:
     # hats = beanie, knitted had
     outfits = [
-        [["beanie", "trapper", "winter headband"], ["beanie", "hood"], ["beanie", "hood", "nothing"], ["cap", "nothing"], ["cap", "nothing"]],
-        [["layers of jacket"], ["jacket"], ["jacket"], ["long sleeve shirt", "turtle neck"], ["t-shirt", "tanktop"]],
-        [["snow pants"], ["jeans"], ["jeans", "baggy pants"], ["jeans","baggy pants", "shorts", "joggers"], ["jeans", "baggy pants", "shorts", "joggers"]],
-        [["snow boots"], ["winter boots"], ["winter boots", "sneakers", "white shoes"], ["white shoes", "flip flops", "running shoes", "dress shoe"], ["white shoes", "flip flops", "running shoes", "dress shoe"]],
-        [["winter gloves"], ["gloves", "mittens"], ["gloves", "mittens", "none"], ["bike gloves", "none"], ["none"]]
+        [
+            ["beanie", "trapper", "winter headband"],
+            ["beanie", "hood"],
+            ["beanie", "hood", "nothing"],
+            ["cap", "nothing"],
+            ["cap", "nothing"]
+        ],
+        [
+            ["layers of jacket"],
+            ["jacket"],
+            ["jacket"],
+            ["long sleeve shirt", "turtle neck"],
+            ["t-shirt", "tanktop"]
+        ],
+        [
+            ["snow pants"],
+            ["jeans"],
+            ["jeans", "baggy pants"],
+            ["jeans", "baggy pants", "shorts", "joggers"],
+            ["jeans", "baggy pants", "shorts", "joggers"]
+        ],
+        [
+            ["snow boots"],
+            ["winter boots"],
+            ["winter boots", "sneakers", "white shoes"],
+            ["white shoes", "flip flops", "running shoes", "dress shoe"],
+            ["white shoes", "flip flops", "running shoes", "dress shoe"]
+        ],
+        [
+            ["winter gloves"],
+            ["gloves", "mittens"],
+            ["gloves", "mittens", "none"],
+            ["bike gloves", "none"],
+            ["none"]
+        ]
     ]
 
     rainoutfits = [
-        [["beanie", "trapper", "winter headband"], ["beanie", "hood"], ["beanie", "hood"], ["cap"], ["cap"]],
-        [["layers of jacket"], ["jacket"], ["jacket"], ["raincoat"], ["raincoat"]],
-        [["snow pants"], ["jeans"], ["jeans", "baggy pants"], ["jeans","baggy pants", "shorts", "joggers"], ["jeans", "baggy pants", "shorts", "joggers"]],
-        [["snow boots"], ["winter boots"], ["winter boots", "sneakers", "white shoes"], ["running shoes"], ["flip flops", "running shoes"]],
-        [["winter gloves"], ["gloves", "mittens"], ["gloves", "mittens", "none"], ["bike gloves", "none"], ["none"]]
+        [
+            ["beanie", "trapper", "winter headband"],
+            ["beanie", "hood"],
+            ["beanie", "hood"],
+            ["cap"],
+            ["cap"]
+        ],
+        [
+            ["layers of jacket"],
+            ["jacket"],
+            ["jacket"],
+            ["raincoat"],
+            ["raincoat"]
+        ],
+        [
+            ["snow pants"],
+            ["jeans"],
+            ["jeans", "baggy pants"],
+            ["jeans", "baggy pants", "shorts", "joggers"],
+            ["jeans", "baggy pants", "shorts", "joggers"]
+        ],
+        [
+            ["snow boots"],
+            ["winter boots"],
+            ["winter boots", "sneakers", "white shoes"],
+            ["running shoes"],
+            ["flip flops", "running shoes"]
+        ],
+        [
+            ["winter gloves"],
+            ["gloves", "mittens"],
+            ["gloves", "mittens", "none"],
+            ["bike gloves", "none"],
+            ["none"]
+        ]
     ]
 
     def pick_outfit(self, set: int, rain: bool) -> List[str]:
