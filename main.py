1da475ac1433d6d2b4ba7b199d7ac85d97c372be
JaPNaA <leonehuang100@gmail.com>
2022-04-30 09:31:08 -0400

    add function send message with unsplash

--- main.py
+++ main.py
@@ -1,23 +1,60 @@
+import os
 import discord
 import urllib.request
 import json
 
+if not os.path.exists("./cache"):
+    os.mkdir("cache")
+
 try:
-    with open(".bot_token") as file:
-        key = file.read().strip()
+    with open("cache/.bot_token") as file:
+        bot_key = file.read().strip()
 except:
-    key = ""
+    bot_key = ""
+
+if not bot_key:
+    bot_key = input("Enter bot token: ")
+    with open("cache/.bot_token", 'w') as file:
+        file.write(bot_key)
+
+try:
+    with open("cache/.weather_token") as file:
+        weather_key = file.read().strip()
+except:
+    weather_key = ""
+
+if not weather_key:
+    weather_key = input("Enter weather token: ")
+    with open("cache/.weather_token", 'w') as file:
+        file.write(weather_key)
 
 client = discord.Client()
 
+cache_file_index = 0
 
 def get_weather():
     # 43.548899,-79.6650758
     response = urllib.request.urlopen(
-        "https://api.openweathermap.org/data/2.5/weather?lat=43.548899&lon=-79.6650758&appid=54fcadf5eb972b7b498158fa814a0841")
+        "https://api.openweathermap.org/data/2.5/weather?lat=43.548899&lon=-79.6650758&appid=" + weather_key)
     response_data = response.read()
     return json.loads(response_data)
 
+
+async def send_message_with_unsplash_image(text, query, channel):
+    global cache_file_index
+
+    image_req = urllib.request.urlopen(
+        "https://source.unsplash.com/random/280x280/?" + query)
+
+    cache_file_index += 1
+    _local_cache_file_index = cache_file_index
+    cache_file_path = f"./cache/image{_local_cache_file_index}.jpg"
+
+    with open(cache_file_path, 'wb') as file:
+        file.write(image_req.read())
+    with open(cache_file_path, 'rb') as file:
+        discord_file = discord.File(file)
+        await channel.send(text, file=discord_file)
 
 @client.event
 async def on_ready():
@@ -32,11 +69,12 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         # await message.channel.send('Hello!')
         await message.channel.send(get_weather())
+    elif message.content.startswith('$unsplash'):
+        await send_message_with_unsplash_image(
+            text='test', 
+            query='hat',
+            channel=message.channel
+        )
 
-if not key:
-    key = input("Enter key: ")
-    with open(".bot_token", 'w') as file:
-        file.write(key)
-
-client.run(key)
+client.run(bot_key)
 
